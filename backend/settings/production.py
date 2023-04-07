409217db25ab7cef973dee22b76ea69b5f6a0faa
Jakub Stastka <jakub@stastka.org>
2023-04-07 13:48:07 +0200

    sentry integration added

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,7 +1,14 @@
 from .base import *
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    integrations=[DjangoIntegration()]
+)
 
 DATABASES = {
     'default': {
