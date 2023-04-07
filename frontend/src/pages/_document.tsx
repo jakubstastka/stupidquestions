a373344a1d1b7ebe51c3cd38ee7fcb20eaaa3545
Jakub Stastka <jakub@stastka.org>
2023-04-07 18:12:44 +0200

    document head edited with open graph data

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+          <meta property="og:title" content="Blbý otázky" />
+          <meta property="og:description" content="AI odpovídá na všechny blbý otázky. Už se nemusíte stydět ptát se na ledasco." />
+          <meta property="og:url" content="https://www.blbyotazky.cz" />
+        </Head>
       <body>
         <Main />
         <NextScript />
