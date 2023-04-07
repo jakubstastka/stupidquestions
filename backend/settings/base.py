4db21f6d7f4710909b40c65f498ce6a7c9b3cde8
Jakub Stastka <jakub@stastka.org>
2023-04-07 14:01:42 +0200

    logging to file set up since sentry isnt working

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -125,3 +125,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+}
